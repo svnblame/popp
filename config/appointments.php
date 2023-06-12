a137eddcc8ad9a7b441953408887f10d97b0d339
Gene Kelley <eugenek@guildmortgage.net>
2023-06-12 16:13:07 -0700

    Add dependency injection (homegrown).

---
+++ config/appointments.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    [
+        'name' => 'TerrainFactory',
+        'args' => [
+            'EarthSea',
+            'MarsPlains',
+            'EarthForest'
+        ]
+    ],
+    [
+        'name' => 'AppointmentMaker',
+        'args' => [
+            'BlogAppointmentEncoder'
+        ]
+    ],
+    [
+        'name' => 'BlogCommunicationsManager',
+        'args'  => []
+    ]
+];
