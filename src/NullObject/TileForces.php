081395757361a19b7ed8cf19693b0a2bd034b622
= <=>
2023-11-01 10:38:23 -0700

    Add Null Object pattern example.

---
+++ src/NullObject/TileForces.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace POPP\NullObject;
+
+class TileForces
+{
+    /**
+     * @var int
+     */
+    private int $x;
+
+    /**
+     * @var int
+     */
+    private int $y;
+
+    /**
+     * @var array
+     */
+    private array $units = [];
+
+    public function __construct(int $x, int $y, UnitAcquisition $acquisition)
+    {
+        $this->x = $x;
+        $this->y = $y;   // @todo Should this be = $x??
+        $this->units = $acquisition->getUnits($this->x, $this->y);
+    }
+
+    public function firePower(): int
+    {
+        $power = 0;
+
+        foreach ($this->units as $unit) {
+            if (! is_null($unit)) {
+                $power += $unit->bombardStrength();
+            }
+        }
+
+        return $power;
+    }
+
+    public function health(): int
+    {
+        $health = 0;
+
+        foreach ($this->units as $unit) {
+            if (! is_null($unit)) {
+                $health += $unit->getHealth();
+            }
+        }
+
+        return $health;
+    }
+}
---
+++ src/NullObject/index.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types =1 );
+
+namespace POPP\NullObject;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+Runner::run();
